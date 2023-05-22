233b9f85ff0815ec5cec6d37414f1fb9d034e5f6
myusername <rabbaidibibata@yahoo.fr>
2023-05-22 19:32:33 -0400

    hw5 part2-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import LinkContainer from './components/LinkContainer'
+import {useEffect, useState} from 'react'
+
 
 function App() {
+
+  const [message, setMessage] = useState(null)
+  useEffect(()=>{
+    // perform my API request
+    // use fetch to get our data
+    fetch("/api")
+    .then((res) => res.json())
+    .then(data => {
+      console.log(data)
+      setMessage(data.message)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+
+  }, [])
+
   return (
     <div className="App">
       <LinkContainer />
